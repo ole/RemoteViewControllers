0989dacfe7139e069182ab141bde3c4b0fc9d196
Ole Begemann <ole@oleb.net>
2012-10-02 15:29:23 +0200

    Use more meaningful types and variable names in swizzled method

--- RemoteViewControllers/_UIRemoteViewController/_UIRemoteViewController+Swizzling.m
+++ RemoteViewControllers/_UIRemoteViewController/_UIRemoteViewController+Swizzling.m
@@ -7,19 +7,20 @@
 //
 
 #import "_UIRemoteViewController+Swizzling.h"
+#import "_UIAsyncInvocation.h"
 
 id (*globalOriginalRequestViewController)(id, SEL, id, id, id);
 
-id SwizzledRequestViewControllerFromServiceWithBundleIdentifierConnectionHandler(_UIRemoteViewController *_self, SEL _cmd, id arg1, id arg2, id arg3)
+_UIAsyncInvocation *SwizzledRequestViewControllerFromServiceWithBundleIdentifierConnectionHandler(_UIRemoteViewController *_self, SEL _cmd, NSString *viewControllerName, NSString *serviceBundleID, id connectionHandlerBlock)
 {
     NSLog(@"%s", __FUNCTION__);
     NSLog(@"self: %@", _self);
-    NSLog(@"arg1: %@ %@", [arg1 class], arg1);
-    NSLog(@"arg2: %@ %@", [arg2 class], arg2);
-    NSLog(@"arg3: %@ %@", [arg3 class], arg3);
+    NSLog(@"arg1: %@ %@", [viewControllerName class], viewControllerName);
+    NSLog(@"arg2: %@ %@", [serviceBundleID class], serviceBundleID);
+    NSLog(@"arg3: %@ %@", [connectionHandlerBlock class], connectionHandlerBlock);
     
     // Call the original implementation of the method
-    id returnValue = globalOriginalRequestViewController(_self, _cmd, arg1, arg2, arg3);
+    _UIAsyncInvocation *returnValue = globalOriginalRequestViewController(_self, _cmd, viewControllerName, serviceBundleID, connectionHandlerBlock);
     
     NSLog(@"Return Value: %@ %@", [returnValue class], returnValue);
     return returnValue;
