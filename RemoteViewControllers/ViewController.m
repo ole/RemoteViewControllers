5f8e3faff4a9c5566d7f1fdc2814d035134c20bc
Ole Begemann <ole@oleb.net>
2012-10-04 19:57:30 +0200

    Add button to open App Store

--- RemoteViewControllers/ViewController.m
+++ RemoteViewControllers/ViewController.m
@@ -13,13 +13,14 @@
 - (NSString *)recursiveDescription;
 @end
 
-@interface ViewController () <MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate, QLPreviewControllerDataSource>
+@interface ViewController () <MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate, QLPreviewControllerDataSource, SKStoreProductViewControllerDelegate>
 
 - (IBAction)openMailComposer:(id)sender;
 - (IBAction)openMessageComposer:(id)sender;
 - (IBAction)openTweetComposer:(id)sender;
 - (IBAction)openFacebookSharing:(id)sender;
 - (IBAction)openQuickLook:(id)sender;
+- (IBAction)openAppStore:(id)sender;
 
 @end
 
@@ -130,6 +131,19 @@
     }];
 }
 
+- (IBAction)openAppStore:(id)sender
+{
+    SKStoreProductViewController *controller = [[SKStoreProductViewController alloc] init];
+    controller.delegate = self;
+    [self presentViewController:controller animated:YES completion:^{
+        // Log the view hierarchy here to see what's going on
+        //NSLog(@"View hierarchy: %@", [controller.view recursiveDescription]);
+    }];
+    [controller loadProductWithParameters:@{ SKStoreProductParameterITunesItemIdentifier : @"361309726" } completionBlock:^(BOOL result, NSError *error) {
+        // Do nothing
+    }];
+}
+
 #pragma mark - MFMailComposeViewControllerDelegate
 
 - (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
@@ -154,6 +168,13 @@
 - (id<QLPreviewItem>)previewController:(QLPreviewController *)controller previewItemAtIndex:(NSInteger)index
 {
     return [[NSBundle mainBundle] URLForResource:@"iphone-5-vertical" withExtension:@"png"];
+}
+
+#pragma mark - SKStoreProductViewControllerDelegate
+
+- (void)productViewControllerDidFinish:(SKStoreProductViewController *)viewController
+{
+    [self dismissModalViewControllerAnimated:YES];
 }
 
 @end
